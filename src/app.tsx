be78c5a5978ff762c5de5b5fe7f58e4e5dd78851
yangyb <yangyb@boyachain.cn>
2023-12-06 13:09:45 +0800

    chore: update pl-vue

--- src/app.tsx
+++ src/app.tsx
@@ -1,29 +1,26 @@
 import { h, Fragment } from "pl-vue";
-import { Link, Router, createRouter } from 'pl-vue/lib/router';
+import { Link, Route, Router, initRouter } from 'pl-vue/lib/router';
 import env from '~/config/env';
 import Home from './pages/home';
 import About from './pages/about';
 import NotFound from "./pages/not-found";
 import style from './style.module.scss';
 
-const router = createRouter({
-  base: env.BASE_URL,
-  mode: 'hash',
-  routes: [
-    { path: '/', component: Home, },
-    { path: '/about', component: About, },
-    { component: NotFound, },
-  ]
-});
-
-
 function App() {
+  const router = initRouter({
+    base: env.BASE_URL,
+    mode: 'hash',
+  });
+
   return <>
+    <Router notFound={NotFound}>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </Router>
     <nav className={style.nav}>
       <Link to='/'>Home</Link>
       <Link to='/about'>About</Link>
     </nav>
-    <Router />
   </>
 }
 
